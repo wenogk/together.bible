ad64179351311b51042119a86d520ddb7b79abe8
wenogk <drf325@nyu.edu>
2020-07-15 16:50:31 +0400

    k

--- js/bible.js
+++ js/bible.js
@@ -60,9 +60,9 @@ window.addEventListener("load", function () {
       getBooks("de4e12af7f28f599-01").then((bookList) => {
         let index = 0;
         for (let book of bookList) {
+          let bookIDAPI = book.id;
           try {
             let bookNameNKJV = FULL_NKJV["books"][index].name;
-            let bookIDAPI = book.id;
             debug += `case "${bookNameNKJV}" : \n return "${bookIDAPI}" \n`;
             index += 1;
           } catch {
